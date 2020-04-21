7cdfd8345ed89da2a033f118e0fafccf6a36c38c
olaven <olav@sundfoer.com>
2020-04-21 16:30:29 +0200

    moved away from arrow functions and improved doc

--- mod.ts
+++ mod.ts
@@ -7,11 +7,30 @@ export interface Tag {
   attributes: [string, string][];
 }
 
-export const tag = (name: string, children: string | Tag[] = "", attributes: [string, string][] = []): Tag => ({
-  name, children, attributes
-})
+/**
+ * Returns a `Tag`
+ * @param name name of the tag 
+ * @param children subtags/content
+ * @param attributes key/value attributes of the tag
+ */
+export function tag(name: string, children: string | Tag[] = "", attributes: [string, string][] = []): Tag {
+  return {name, children, attributes}
+}
 
-const get_content = (tag: Tag) =>
+/**
+ * Serializes given tag and its children 
+ * to an XML string.  
+ * @param {Tag} tag the tag to serialize 
+ */
+export function serialize(tag: Tag): string {
+  const content = get_children(tag);
+  const attributes = format_attributes(tag);
+
+  return format_tag(tag.name, attributes, content);
+};
+
+
+const get_children = (tag: Tag) =>
   (typeof (tag.children) === "string")
     ? tag.children
     : tag.children
@@ -27,15 +46,3 @@ const format_tag = (name: string, attributes: string, content: string) =>
   `<${name}${attributes.length > 0
     ? ` ${attributes}`
     : ``}>${content}</${name}>`;
-
-/**
- * Serializes given tag and its children 
- * to an XML string.  
- * @param {Tag} tag the tag to serialize 
- */
-export const serialize = (tag: Tag): string => {
-  const content = get_content(tag);
-  const attributes = format_attributes(tag);
-
-  return format_tag(tag.name, attributes, content);
-};
